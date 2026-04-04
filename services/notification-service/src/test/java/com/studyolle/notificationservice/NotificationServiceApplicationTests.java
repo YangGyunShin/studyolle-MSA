45ce984419000b9b879e700d88ff0ec1a784a21a
YangGyunShin <skshtlsdidrb@gmail.com>
2026-04-04 16:15:36 +0900

    update

---
+++ services/notification-service/src/test/java/com/studyolle/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyolle.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
